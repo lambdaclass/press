92a634e649e001debe613bf7b404a0e0ad2b1417
Manuel Rubio <manuel@altenwald.com>
2026-07-21 01:50:34 +0200

    Add PDF object assembly, xref/trailer, and Writer.to_binary/1

---
+++ lib/press/pdf/writer.ex
@@ -0,0 +1,108 @@
+defmodule Press.PDF.Writer do
+  @moduledoc false
+
+  alias Press.PDF.{ContentStream, Document, Fonts, Syntax}
+
+  def to_binary(%Document{pages: pages}) do
+    page_count = length(pages)
+    fonts = collect_fonts(pages)
+    first_font_obj_num = 3 + page_count * 2
+
+    font_resource_names =
+      fonts |> Enum.with_index(1) |> Map.new(fn {font, i} -> {font, "/F#{i}"} end)
+
+    font_obj_numbers = fonts |> Enum.with_index(first_font_obj_num) |> Map.new()
+
+    resources = resources_dict(fonts, font_resource_names, font_obj_numbers)
+
+    page_obj_numbers =
+      if page_count == 0, do: [], else: for(i <- 0..(page_count - 1), do: 3 + i * 2)
+
+    page_and_content_objects =
+      pages
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {page, i} ->
+        page_obj_num = 3 + i * 2
+        contents_obj_num = 4 + i * 2
+        content_bytes = ContentStream.render(page.ops, font_resource_names)
+
+        page_body =
+          "<< /Type /Page /Parent 2 0 R " <>
+            "/MediaBox [0 0 #{Syntax.number(page.width)} #{Syntax.number(page.height)}] " <>
+            "/Resources #{resources} /Contents #{contents_obj_num} 0 R >>"
+
+        [{page_obj_num, page_body}, {contents_obj_num, stream_body(content_bytes)}]
+      end)
+
+    catalog = {1, "<< /Type /Catalog /Pages 2 0 R >>"}
+
+    kids = Enum.map_join(page_obj_numbers, " ", &"#{&1} 0 R")
+    pages_obj = {2, "<< /Type /Pages /Kids [#{kids}] /Count #{page_count} >>"}
+
+    font_objects =
+      for font <- fonts do
+        {font_obj_numbers[font],
+         "<< /Type /Font /Subtype /Type1 /BaseFont /#{Fonts.base_font_name(font)} >>"}
+      end
+
+    objects =
+      ([catalog, pages_obj] ++ page_and_content_objects ++ font_objects)
+      |> Enum.sort_by(&elem(&1, 0))
+
+    assemble("%PDF-1.4\n", objects)
+  end
+
+  defp collect_fonts(pages) do
+    pages
+    |> Enum.flat_map(fn page ->
+      Enum.flat_map(page.ops, fn
+        {:text, _x, _y, font, _size, _color, _text} -> [font]
+        _other -> []
+      end)
+    end)
+    |> Enum.uniq()
+  end
+
+  defp resources_dict(fonts, font_resource_names, font_obj_numbers) do
+    entries =
+      Enum.map_join(fonts, " ", fn font ->
+        "#{font_resource_names[font]} #{font_obj_numbers[font]} 0 R"
+      end)
+
+    "<< /Font << #{entries} >> >>"
+  end
+
+  defp stream_body(content_bytes) do
+    length = IO.iodata_length(content_bytes)
+    "<< /Length #{length} >>\nstream\n#{IO.iodata_to_binary(content_bytes)}\nendstream"
+  end
+
+  defp assemble(header, objects) do
+    {body_iodata, offsets} =
+      Enum.reduce(objects, {[], %{}}, fn {num, body}, {acc, offsets} ->
+        offset = byte_size(header) + IO.iodata_length(acc)
+        obj_iodata = "#{num} 0 obj\n#{body}\nendobj\n"
+        {[acc, obj_iodata], Map.put(offsets, num, offset)}
+      end)
+
+    xref_offset = byte_size(header) + IO.iodata_length(body_iodata)
+    max_obj_num = objects |> Enum.map(&elem(&1, 0)) |> Enum.max()
+
+    xref_entries =
+      for num <- 1..max_obj_num do
+        pad_offset(Map.fetch!(offsets, num)) <> " 00000 n\r\n"
+      end
+
+    xref =
+      "xref\n0 #{max_obj_num + 1}\n" <>
+        "0000000000 65535 f\r\n" <>
+        Enum.join(xref_entries)
+
+    trailer =
+      "trailer\n<< /Size #{max_obj_num + 1} /Root 1 0 R >>\nstartxref\n#{xref_offset}\n%%EOF"
+
+    IO.iodata_to_binary([header, body_iodata, xref, trailer])
+  end
+
+  defp pad_offset(offset), do: offset |> Integer.to_string() |> String.pad_leading(10, "0")
+end
---
+++ test/press/pdf/writer_test.exs
@@ -0,0 +1,67 @@
+defmodule Press.PDF.WriterTest do
+  use ExUnit.Case, async: true
+
+  alias Press.PDF.{Document, Writer}
+
+  test "starts with the PDF header" do
+    binary =
+      Document.new()
+      |> then(fn doc -> elem(Document.add_page(doc, 595.0, 842.0), 0) end)
+      |> Writer.to_binary()
+
+    assert String.starts_with?(binary, "%PDF-1.4\n")
+  end
+
+  test "includes a Pages object with the right /Count for multiple pages" do
+    doc = Document.new()
+    {doc, _} = Document.add_page(doc, 595.0, 842.0)
+    {doc, _} = Document.add_page(doc, 595.0, 842.0)
+
+    binary = Writer.to_binary(doc)
+
+    assert binary =~ "/Type /Pages /Kids [3 0 R 5 0 R] /Count 2"
+  end
+
+  test "includes exactly one Font object per distinct font used, shared across pages" do
+    doc = Document.new()
+    {doc, p0} = Document.add_page(doc, 595.0, 842.0)
+    {doc, p1} = Document.add_page(doc, 595.0, 842.0)
+    doc = Document.draw_text(doc, p0, 0.0, 0.0, "a", font: :helvetica)
+    doc = Document.draw_text(doc, p1, 0.0, 0.0, "b", font: :helvetica)
+
+    binary = Writer.to_binary(doc)
+
+    assert length(Regex.scan(~r{/BaseFont /Helvetica\b}, binary)) == 1
+  end
+
+  test "every xref offset points at the matching \"N 0 obj\"" do
+    doc = Document.new()
+    {doc, p} = Document.add_page(doc, 595.0, 842.0)
+    doc = Document.draw_text(doc, p, 10.0, 20.0, "Hello world!", font: :helvetica_bold, size: 24)
+
+    binary = Writer.to_binary(doc)
+
+    [_before, xref_and_trailer] = String.split(binary, "xref\n", parts: 2)
+    [entries_block, _trailer] = String.split(xref_and_trailer, "trailer\n", parts: 2)
+    ["0 " <> _count | entry_lines] = String.split(entries_block, "\r\n", trim: true)
+
+    entry_lines
+    |> Enum.with_index(1)
+    |> Enum.each(fn {line, obj_num} ->
+      offset = line |> String.slice(0, 10) |> String.to_integer()
+      following = binary |> String.slice(offset, byte_size("#{obj_num} 0 obj"))
+      assert following == "#{obj_num} 0 obj"
+    end)
+  end
+
+  test "ends with a valid trailer pointing at object 1 as Root" do
+    doc = Document.new()
+    {doc, _} = Document.add_page(doc, 595.0, 842.0)
+
+    binary = Writer.to_binary(doc)
+
+    assert binary =~ "trailer\n<< /Size"
+    assert binary =~ "/Root 1 0 R"
+    assert String.ends_with?(binary, "%%EOF")
+  end
+end

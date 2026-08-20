08090d352620d9c39587c66ed9d6dc58021abf34
Manuel Rubio <manuel@altenwald.com>
2026-08-20 05:35:30 +0200

    Add Press.Layout.Table for table column and row layout

---
+++ lib/press/layout/table.ex
@@ -0,0 +1,187 @@
+defmodule Press.Layout.Table do
+  @moduledoc false
+
+  alias Press.Layout.{Block, Box}
+  alias Press.Style.Node
+
+  @doc """
+  Lays out a <table> node.
+  Returns `{table_box, next_y, margin_bottom}`.
+  """
+  def layout_table(%Node{} = table_node, containing_width, container_x, start_y) do
+    computed = table_node.computed
+
+    margin = resolve_box_dimensions(computed.margin, containing_width)
+    padding = resolve_box_dimensions(computed.padding, containing_width)
+    border_width = resolve_box_dimensions(computed.border_width, containing_width)
+
+    table_width =
+      case computed.width do
+        {:percent, p} ->
+          p / 100.0 * containing_width
+
+        n when is_number(n) ->
+          n * 1.0
+
+        :auto ->
+          max(
+            0.0,
+            containing_width - margin.left - margin.right - padding.left - padding.right -
+              border_width.left - border_width.right
+          )
+      end
+
+    table_x = container_x + margin.left
+    table_y = start_y + margin.top
+
+    content_origin_x = table_x + border_width.left + padding.left
+    content_origin_y = table_y + border_width.top + padding.top
+
+    row_nodes = extract_rows(table_node.children)
+
+    col_count = count_columns(row_nodes)
+    column_widths = calculate_column_widths(row_nodes, col_count, table_width)
+
+    {row_boxes, total_height} =
+      Enum.reduce(row_nodes, {[], 0.0}, fn row_node, {r_acc, curr_y} ->
+        {row_box, row_height} =
+          layout_row(row_node, column_widths, content_origin_x, content_origin_y + curr_y)
+
+        {[row_box | r_acc], curr_y + row_height}
+      end)
+
+    table_box = %Box{
+      type: :table,
+      tag: "table",
+      x: table_x,
+      y: table_y,
+      width: table_width,
+      height: total_height,
+      margin: margin,
+      padding: padding,
+      border_width: border_width,
+      border_color: computed.border_color,
+      border_style: computed.border_style,
+      background_color: computed.background_color,
+      computed: computed,
+      children: Enum.reverse(row_boxes)
+    }
+
+    next_y =
+      table_y + total_height + padding.top + padding.bottom + border_width.top +
+        border_width.bottom
+
+    {table_box, next_y, margin.bottom}
+  end
+
+  defp extract_rows(children) do
+    Enum.flat_map(children, fn
+      %Node{element: %{tag: "tr"}} = tr ->
+        [tr]
+
+      %Node{element: %{tag: section}, children: section_children}
+      when section in ["thead", "tbody", "tfoot"] ->
+        extract_rows(section_children)
+
+      _ ->
+        []
+    end)
+  end
+
+  defp count_columns(row_nodes) do
+    row_nodes
+    |> Enum.map(fn %Node{children: cells} ->
+      Enum.count(cells, fn %Node{element: %{tag: tag}} -> tag in ["th", "td"] end)
+    end)
+    |> Enum.max(fn -> 0 end)
+  end
+
+  defp calculate_column_widths(_row_nodes, 0, table_width), do: [table_width]
+
+  defp calculate_column_widths(row_nodes, col_count, table_width) do
+    explicit_widths =
+      for c <- 0..(col_count - 1) do
+        row_nodes
+        |> Enum.find_value(nil, fn %Node{children: cells} ->
+          case Enum.at(cells, c) do
+            %Node{computed: %{width: {:percent, p}}} ->
+              p / 100.0 * table_width
+
+            %Node{computed: %{width: n}} when is_number(n) ->
+              n * 1.0
+
+            _ ->
+              nil
+          end
+        end)
+      end
+
+    total_explicit = explicit_widths |> Enum.reject(&is_nil/1) |> Enum.sum()
+    unspecified_count = Enum.count(explicit_widths, &is_nil/1)
+
+    default_col_width =
+      if unspecified_count > 0 do
+        max(0.0, table_width - total_explicit) / unspecified_count
+      else
+        0.0
+      end
+
+    Enum.map(explicit_widths, fn
+      nil -> default_col_width
+      w -> w
+    end)
+  end
+
+  defp layout_row(%Node{} = row_node, column_widths, origin_x, row_y) do
+    cells =
+      Enum.filter(row_node.children, fn %Node{element: %{tag: tag}} -> tag in ["th", "td"] end)
+
+    {cell_boxes, _final_x} =
+      cells
+      |> Enum.with_index()
+      |> Enum.reduce({[], origin_x}, fn {cell_node, c_idx}, {c_acc, curr_x} ->
+        col_width = Enum.at(column_widths, c_idx, 0.0)
+
+        {cell_box, _next_y, _m} = Block.layout_block(cell_node, col_width, curr_x, row_y)
+
+        cell_box = %Box{cell_box | type: :table_cell, width: col_width, x: curr_x, y: row_y}
+
+        {[cell_box | c_acc], curr_x + col_width}
+      end)
+
+    cell_boxes = Enum.reverse(cell_boxes)
+    row_height = Enum.map(cell_boxes, & &1.height) |> Enum.max(fn -> 0.0 end)
+
+    stretched_cells =
+      Enum.map(cell_boxes, fn cell ->
+        %Box{cell | height: row_height}
+      end)
+
+    row_width = Enum.sum(column_widths)
+
+    row_box = %Box{
+      type: :table_row,
+      tag: "tr",
+      x: origin_x,
+      y: row_y,
+      width: row_width,
+      height: row_height,
+      children: stretched_cells
+    }
+
+    {row_box, row_height}
+  end
+
+  defp resolve_box_dimensions(%{top: t, right: r, bottom: b, left: l}, containing_width) do
+    %{
+      top: resolve_dimension(t, containing_width),
+      right: resolve_dimension(r, containing_width),
+      bottom: resolve_dimension(b, containing_width),
+      left: resolve_dimension(l, containing_width)
+    }
+  end
+
+  defp resolve_dimension({:percent, p}, containing_width), do: p / 100.0 * containing_width
+  defp resolve_dimension(n, _containing_width) when is_number(n), do: n * 1.0
+  defp resolve_dimension(_, _), do: 0.0
+end
---
+++ test/press/layout/table_test.exs
@@ -0,0 +1,124 @@
+defmodule Press.Layout.TableTest do
+  use ExUnit.Case, async: true
+
+  alias Press.Layout.Table
+  alias Press.Style.{Node, Text}
+
+  defp table_node(rows) do
+    %Node{
+      element: %Press.HTML.Element{tag: "table", attrs: %{}, children: []},
+      computed: default_computed(),
+      children: rows
+    }
+  end
+
+  defp row_node(cells) do
+    %Node{
+      element: %Press.HTML.Element{tag: "tr", attrs: %{}, children: []},
+      computed: default_computed(),
+      children: cells
+    }
+  end
+
+  defp cell_node(tag, content, width \\ :auto) do
+    %Node{
+      element: %Press.HTML.Element{tag: tag, attrs: %{}, children: []},
+      computed: Map.put(default_computed(), :width, width),
+      children: [
+        %Text{
+          content: content,
+          computed: default_computed()
+        }
+      ]
+    }
+  end
+
+  defp default_computed do
+    %{
+      color: {0.0, 0.0, 0.0},
+      font_family: :helvetica,
+      font_size: 12.0,
+      font_weight: :normal,
+      font_style: :normal,
+      line_height: 14.4,
+      text_align: :left,
+      margin: %{top: 0.0, right: 0.0, bottom: 0.0, left: 0.0},
+      padding: %{top: 0.0, right: 0.0, bottom: 0.0, left: 0.0},
+      border_width: %{top: 0.0, right: 0.0, bottom: 0.0, left: 0.0},
+      border_color: %{top: nil, right: nil, bottom: nil, left: nil},
+      border_style: %{top: :none, right: :none, bottom: :none, left: :none},
+      background_color: nil,
+      width: :auto,
+      height: :auto
+    }
+  end
+
+  describe "layout_table/4" do
+    test "distributes table width evenly across columns when no widths are specified" do
+      rows = [
+        row_node([cell_node("th", "Col 1"), cell_node("th", "Col 2")]),
+        row_node([cell_node("td", "A"), cell_node("td", "B")])
+      ]
+
+      table = table_node(rows)
+
+      # Table width = 300.0, 2 columns -> each column is 150.0 wide
+      {table_box, next_y, _margin_bottom} = Table.layout_table(table, 300.0, 0.0, 0.0)
+
+      assert table_box.type == :table
+      assert table_box.width == 300.0
+      assert length(table_box.children) == 2
+
+      [r1, r2] = table_box.children
+      [c1_1, c1_2] = r1.children
+      [c2_1, c2_2] = r2.children
+
+      assert c1_1.width == 150.0
+      assert c1_1.x == 0.0
+      assert c1_2.width == 150.0
+      assert c1_2.x == 150.0
+
+      assert c2_1.width == 150.0
+      assert c2_2.width == 150.0
+
+      assert next_y > 0.0
+    end
+
+    test "respects explicit percentage or point column widths" do
+      rows = [
+        row_node([
+          cell_node("td", "30%", {:percent, 30.0}),
+          cell_node("td", "70%", {:percent, 70.0})
+        ])
+      ]
+
+      table = table_node(rows)
+      {table_box, _next_y, _margin_bottom} = Table.layout_table(table, 400.0, 0.0, 0.0)
+
+      [row] = table_box.children
+      [c1, c2] = row.children
+
+      assert c1.width == 120.0  # 30% of 400
+      assert c2.width == 280.0  # 70% of 400
+    end
+
+    test "synchronizes cell heights across the same row to the maximum cell height" do
+      # In Courier 10pt (char width 6pt), line height 12pt:
+      # "Short" (30pt) in 60pt column -> 1 line (14.4pt)
+      # "Line 1 Line 2 Line 3" in 60pt column -> wraps into 3 lines (43.2pt)
+      c1 = cell_node("td", "Short", 60.0)
+      c2 = cell_node("td", "Line 1 Line 2 Line 3", 60.0)
+
+      rows = [row_node([c1, c2])]
+      table = table_node(rows)
+
+      {table_box, _next_y, _margin_bottom} = Table.layout_table(table, 200.0, 0.0, 0.0)
+
+      [row] = table_box.children
+      [cell1, cell2] = row.children
+
+      assert cell1.height == cell2.height
+      assert cell1.height > 14.4
+    end
+  end
+end
